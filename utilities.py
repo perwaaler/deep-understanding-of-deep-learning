d66008c0e8842f26ca6d4bd4dc9b079a290ed31b
Per Niklas Waaler <perwaaler@gmail.com>
2024-10-12 13:01:55 +0200

    binary_classification.py task done - 3 classes predicted (simulated cluster data)

---
+++ utilities.py
@@ -0,0 +1,50 @@
+RESET_COLOR = "\033[0m"
+COLOR_TO_ANSI_MAP = {  # All are in bold font
+    "grey": "\033[1;30m",
+    "grey_bold": "\033[1;30m",
+    "green": "\033[1;92m",
+    "blue": "\033[1;94m",
+    "orange": "\033[1;33m",
+    "red": "\033[1;91m",
+    "reset": "\033[0m",
+}
+
+
+# %% PRINTING UTILITES
+
+
+def color_text(text: str, color="grey"):
+    """Returns text in the desired colour (grey, green, or blue)."""
+    return f"{COLOR_TO_ANSI_MAP[color]}{text}{RESET_COLOR}"
+
+
+def red_text(text: str):
+    """Returns text in red."""
+    return color_text(text, "red")
+
+
+def color_print(text: str, color="green"):
+    """Prints text in the specified color."""
+    print(color_text(text, color))
+
+
+def silent_print(text: str):
+    """Prints text in console in dark grey."""
+    print(color_text(text, "grey"))
+
+
+def orange_print(text: str):
+    """Prints text (warning) in console in a color close to orange (bold
+    yellow)."""
+    print(color_text(text, "orange"))
+
+
+def green_print(text: str):
+    """Prints text (warning) in console in a color close to orange (bold
+    yellow)."""
+    print(color_text(text, "green"))
+
+
+def red_print(text):
+    """Prints the ValueError message in red."""
+    print(color_text(text, "red"))
